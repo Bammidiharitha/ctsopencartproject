ec319bf5e2a4bd2a52c6f45ebef547b3e0f2f2d5
DELL <DELL@DESKTOP-AD8IFTF>
2020-03-27 17:30:56 +0000

    driver

--- cartproject1/src/test/java/com/TestRunner/addtocartTR.java
+++ cartproject1/src/test/java/com/TestRunner/addtocartTR.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 @CucumberOptions(
 		features = "src/main/resources/feature/testcase.feature",
 		plugin = {"pretty","html:test-output","json:json_output/cucmber.json","junit:junit_xml/cucumber.xml","com.cucumber.listener.ExtentCucumberFormatter:reports/Extentreports/Extentreport.html"},//reports generated
-       //  tags = {"@TC01_AddtoCart,@TC02_Checkcart,@TC03_priceupdate,@TC04_storingProductDetails,@TC05_RemovefromCart"},
+       tags = {"@TC01_AddtoCart,@TC02_Checkcart,@TC03_priceupdate,@TC04_storingProductDetails,@TC05_RemovefromCart"},
          
 		glue = {"com.StepDefinition"},
 		monochrome = true
